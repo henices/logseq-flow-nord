c1dcdd14238469172224fa193ed798fe78860705
henices <henices@163.com>
2023-09-08 13:36:55 +0800

    delete import local css statement

--- gen_custom_css.sh
+++ gen_custom_css.sh
@@ -3,4 +3,4 @@
 cat src/code/base.css src/code/markdown.css src/code/logseq.css src/code/live-query.css src/code/interface.css src/code/plugins.css > custom.css
 cat src/palettes/nord.css >> custom.css
 sed -i '$a a[priority]:not(a[priority=""]), a.priority { font-size: var(--ls-font-size) !important; margin-right: 0.5em !important;}' custom.css
-sed -i '1,5d' custom.css
+sed -i "/@import '/d" custom.css
