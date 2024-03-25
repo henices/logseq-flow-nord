3eb710235065fceac677433fdb6e6135b0d351f6
henices <zhouzhenster@gmail.com>
2024-03-25 22:01:13 +0800

    macOS compatitable

--- gen_custom_css.sh
+++ gen_custom_css.sh
@@ -4,41 +4,41 @@
 if [ $1 == "custom" ]; then
     cat src/code/base.css src/code/markdown.css src/code/logseq.css src/code/live-query.css src/code/interface.css src/code/plugins.css > custom.css
     cat src/palettes/nord-custom.css >> custom.css
-    sed -i "/@import ['\"]/d" custom.css
+    sed -i.bak "/@import ['\"]/d" custom.css
 elif [ $1 == "nord" ]; then
     cat src/code/base.css src/code/markdown.css src/code/logseq.css src/code/live-query.css src/code/interface.css src/code/plugins.css > nord.css
     cat src/palettes/nord.css >> nord.css
-    sed -i "/@import ['\"]/d" nord.css
+    sed -i.bak "/@import ['\"]/d" nord.css
 elif [ $1 == 'sepia' ]; then
     cat src/code/base.css src/code/markdown.css src/code/logseq.css src/code/live-query.css src/code/interface.css src/code/plugins.css > custom.css
     cat src/palettes/nord-custom.css >> custom.css
     cat custom.css src/palettes/sepia.css > sepia.css
-    sed -i "/@import ['\"]/d" sepia.css
+    sed -i.bak "/@import ['\"]/d" sepia.css
 elif [ $1 == 'xiaobot' ]; then
     cat src/code/base.css src/code/markdown.css src/code/logseq.css src/code/live-query.css src/code/interface.css src/code/plugins.css > custom.css
     cat src/palettes/nord-custom.css >> custom.css
     cat custom.css src/palettes/xiaobot.css > xiaobot.css
-    sed -i "/@import ['\"]/d" xiaobot.css
+    sed -i.bak "/@import ['\"]/d" xiaobot.css
 elif [ $1 == 'latte' ]; then
     cat src/code/base.css src/code/markdown.css src/code/logseq.css src/code/live-query.css src/code/interface.css src/code/plugins.css > custom.css
     cat src/palettes/nord-custom.css >> custom.css
     cat custom.css src/palettes/catppuccin-latte.css > latte.css
-    sed -i "/@import ['\"]/d" latte.css
+    sed -i.bak "/@import ['\"]/d" latte.css
 elif [ $1 == 'onedark' ]; then
     cat src/code/base.css src/code/markdown.css src/code/logseq.css src/code/live-query.css src/code/interface.css src/code/plugins.css > custom.css
     cat src/palettes/nord-custom.css >> custom.css
     cat custom.css src/palettes/one-dark-pro.css > onedark.css
-    sed -i "/@import ['\"]/d" onedark.css
+    sed -i.bak "/@import ['\"]/d" onedark.css
 elif [ $1 == 'macchiato' ]; then
     cat src/code/base.css src/code/markdown.css src/code/logseq.css src/code/live-query.css src/code/interface.css src/code/plugins.css > custom.css
     cat src/palettes/nord-custom.css >> custom.css
     cat custom.css src/palettes/catppuccin-macchiato.css > macchiato.css
-    sed -i "/@import ['\"]/d" macchiato.css
+    sed -i.bak "/@import ['\"]/d" macchiato.css
 elif [ $1 == 'dracula' ]; then
     cat src/code/base.css src/code/markdown.css src/code/logseq.css src/code/live-query.css src/code/interface.css src/code/plugins.css > custom.css
     cat src/palettes/nord-custom.css >> custom.css
     cat custom.css src/palettes/dracula.css > dracula.css
-    sed -i "/@import ['\"]/d" dracula.css
+    sed -i.bak "/@import ['\"]/d" dracula.css
 fi
 
-sed -i "/@import ['\"]/d" custom.css
+sed -i.bak "/@import ['\"]/d" custom.css
